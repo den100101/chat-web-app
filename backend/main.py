d28638c715b78a7ae1f68bdf7ccd1122d8394d0b
den100101 <badeodenmark4@gmail.com>
2026-05-23 22:43:56 +0800

    prepare backend for render

--- backend/main.py
+++ backend/main.py
@@ -520,4 +520,4 @@ def uploaded_file(filename):
     return send_from_directory('uploads', filename)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="localhost", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000)
