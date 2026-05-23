2fce21a0d09cf0fab438439ceea215fa279b9569
den100101 <badeodenmark4@gmail.com>
2026-05-24 01:03:52 +0800

    Update task.jsx

--- frontend/src/task.jsx
+++ frontend/src/task.jsx
@@ -14,7 +14,7 @@ function Task() {
 
   async function fetchTasks() {
     try {
-      const response = await fetch("http://localhost:5000/get_tasks");
+      const response = await fetch("http://fabioss.onrender.com/get_tasks");
       const data = await response.json();
       setTasks(data);
     } catch (error) {
@@ -28,7 +28,7 @@ function Task() {
 
   async function handleDelete(id) {
     try {
-      const response = await fetch(`http://localhost:5000/delete_tasks/${id}`, {
+      const response = await fetch(`http://fabioss.onrender.com/delete_tasks/${id}`, {
         method: "DELETE",
       });
 
