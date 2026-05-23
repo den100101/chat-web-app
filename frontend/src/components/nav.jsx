38dd3a63ceffe4d06f3d3298cf5dc3162e33c240
den100101 <badeodenmark4@gmail.com>
2026-05-24 01:07:13 +0800

    Update nav.jsx

--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -12,7 +12,7 @@ function Navigation({ isLoggedin, setisLoggedin }) {
   async function handleLogout(e) {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/logout", {
+    const response = await fetch("http://fabioss.onrender.com/logout", {
       method: "POST",
       credentials: "include",
     });
