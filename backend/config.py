66e5729f856d95ead74efeb9facf89c9b7db85d0
den100101 <badeodenmark4@gmail.com>
2026-05-23 23:00:44 +0800

    making config ready for deployment

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,11 @@
 import mysql.connector
+import os
 
 def get_connection():
     return mysql.connector.connect(
-        host = 'localhost',
-        user = 'root',
-        password = 'admin123',
-        database = 'Fabios'
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=os.getenv("MYSQLPORT")
     )
